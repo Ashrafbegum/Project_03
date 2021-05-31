04ddab6eedb029d8e32d743ee541491743b2f311
Ashraf <ashrafunnisa931@gmail.com>
2021-05-31 16:47:45 +1000

    Created get and post requests

--- index.js
+++ index.js
@@ -3,38 +3,45 @@ const app = express()
 const port = process.env.PORT || 3000
 const data = require('./data.js')
 const bcrypt = require('bcrypt')
+const e = require('express')
 const saltRounds = 10
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
-
-app.get('/', (req, res) => {
+//Returns welcome message
+app.get('/', (req, res) => {  
   res.send('Welcome to our schedule website')
   console.log(data) // Prints entire data
 })
 
+//Returns the list of users
 app.get('/users', (req, res) => {
   console.log(data.users)
   res.send(data.users)
 })
 
+//Returns the list of schedules
 app.get('/schedules', (req, res) => {
   console.log(data.schedules)
   res.send(data.schedules )
 })
 
+//Returns the information of the given user id 
 app.get('/users/:id', (req, res) => {
-  if(req.params.id> data.users.length)
-  res.send('id is not valid')
-  res.send(data.users[req.params.id])
+  if(isUserIdValid(req.params.id)) {
+       if(req.params.id > data.users.length)
+             res.send('User id: ' + req.params.id + ' does not exist')
+       res.send(data.users[req.params.id])
+  } 
+  else 
+       res.send("Please enter a number for the user id.")
 })
 
+//Returns a list of all schedules for the given user id
 app.get('/users/:id/schedules', (req, res) => { 
-    var id = Number(req.params.id) 
-  
-    // NaN() returns true if input is not a number - typeof() returns the string number
-    if(typeof id === 'number' && !isNaN(id)) {   
+    var id = Number(req.params.id)  
+    if(isUserIdValid(req.params.id)) {
       var output = []
       var j=0
   
@@ -49,13 +56,14 @@ app.get('/users/:id/schedules', (req, res) => {
       if(output.length > 0) 
          res.send(output)
       else
-         res.send('Schedules not found for the id:' + req.params.id)   
+         res.send('Schedules not found for the user id: ' + req.params.id)   
        
     }
       else 
         res.send("Please enter a number for the user id.")
 })
-      
+
+//Adds a new user      
 app.post('/users', (req, res) => {
      const pwd = req.body.password
      bcrypt.hash(pwd, saltRounds, (err,hash) => {
@@ -65,15 +73,24 @@ app.post('/users', (req, res) => {
      res.send(req.body)
 })
 
+//Adds a new schedule
 app.post('/schedules', (req, res) => {
   data.schedules.push(req.body)
-  console.log(data.schedules)
   res.send(req.body)
 })
 
 app.get('/users/:id/schedules/:schId', (req, res) => {
   res.send(req.params) //It returns output in key:value format eg., id:0, schId:0
 })
+
+function isUserIdValid(userId) {
+  var id = Number(userId) 
+  
+  // NaN() returns true if input is not a number - typeof() returns the string number
+  if (typeof id === 'number' && !isNaN(id)) 
+      return true;
+  return false
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
